e4acaf2dbcc02867cdd189da0d17902c0370fcf5
wjankowska <weronika.jankowska098@gmail.com>
2019-06-04 21:31:42 +0200

    merge

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,5 +1,6 @@
 import Game from "./game.js";
 
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 // import { formatDate } from "./utility";
 
@@ -23,10 +24,26 @@ const classes = {
     welcomePanelHidden: 'hidden'
 }
 >>>>>>> 3395be2bc313910fc040a9c7bc3721ca1014485d
+=======
+// import { formatDate } from "./utility";
+
+const classes = {
+  errorMsgVisible: "visible",
+  welcomePanelHidden: "hidden"
+};
+
+const sock = io();
+let game = new Game(sock);
+
+const formatDate = date => {
+  return `${date.slice(0, 10)} ${date.slice(11, 16)}`;
+};
+>>>>>>> Stashed changes
 
 let userRegistered = false;
 let name = "";
 
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 const registerName = e => {
   e.preventDefault();
@@ -64,23 +81,28 @@ const receiveMsgToAll = msg => {
 =======
 const registerName = (e) => {
     e.preventDefault();
+=======
+const registerName = e => {
+  e.preventDefault();
+>>>>>>> Stashed changes
 
-    let input = document.getElementById("name-input");
-    let errorMsg = document.getElementsByClassName('error-msg')[0];
+  let input = document.getElementById("name-input");
+  let errorMsg = document.getElementsByClassName("error-msg")[0];
 
-    const name = input.value;
-    input.value = "";
+  const name = input.value;
+  input.value = "";
 
-    if (name.length < 3) {
-        errorMsg.innerHTML = "Name is too short! Type at least 3 characters."
-        errorMsg.classList.add(classes.errorMsgVisible);
-    } else {
-        errorMsg.classList.remove(classes.errorMsgVisible);
-        sock.emit('name', name);
-    }
-}
+  if (name.length < 3) {
+    errorMsg.innerHTML = "Name is too short! Type at least 3 characters.";
+    errorMsg.classList.add(classes.errorMsgVisible);
+  } else {
+    errorMsg.classList.remove(classes.errorMsgVisible);
+    sock.emit("name", name);
+  }
+};
 
 // msg : string ;
+<<<<<<< Updated upstream
 const showsuccessMsg = (msg) => {
     const welcomePanel = document.getElementsByClassName('welcome-panel')[0];
 
@@ -96,12 +118,29 @@ const receiveMsgToAll = (msg) => {
     child.innerHTML =
         `<span class="date">
 >>>>>>> 3395be2bc313910fc040a9c7bc3721ca1014485d
+=======
+const registerSuccessRes = msg => {
+  const welcomePanel = document.getElementsByClassName("welcome-background")[0];
+
+  welcomePanel.classList.add("hidden");
+  userRegistered = true;
+  console.log(msg);
+  game.startGame();
+};
+
+const receiveMsgToAll = msg => {
+  console.log("tutaj");
+  const parent = document.getElementById("chat-list");
+  const child = document.createElement("li");
+  child.innerHTML = `<span class="date">
+>>>>>>> Stashed changes
             ${formatDate(msg.date)}
         </span>
         <span class="message">
             ${msg.text}
         </span>`;
 
+<<<<<<< Updated upstream
 <<<<<<< HEAD
   parent.appendChild(child);
 };
@@ -122,25 +161,36 @@ const newCardAll = () => {};
 =======
     parent.appendChild(child);
 }
+=======
+  parent.appendChild(child);
+};
+>>>>>>> Stashed changes
 
 // new card events
 
-const newCard = (card) => {
-    console.log("you have taken a card " + card.cardCode);
-}
+// const newCard = card => {
+//   if (success === true) {
+//     console.log(card, "you have taken a card " + card.cardCode);
+//   }
+// };
 
-const initNewCard = () => {
-    sock.emit('make-turn', { move: 'new-card' })
-}
+// const initNewCard = () => {
+//   sock.emit("make-turn", { move: "new-card" });
+// };
 
+<<<<<<< Updated upstream
 const newCardAll = () => {
 
 }
 >>>>>>> 3395be2bc313910fc040a9c7bc3721ca1014485d
+=======
+const newCardAll = () => {};
+>>>>>>> Stashed changes
 
 // reshuffle events
 
 const initReshuffle = () => {
+<<<<<<< Updated upstream
 <<<<<<< HEAD
   sock.emit("make-turn", { move: "reshuffle" });
 };
@@ -186,12 +236,17 @@ sock.on("card-thrown-all", cardThrownAll);
 =======
     sock.emit('make-turn', { move: 'reshuffle' })
 }
+=======
+  sock.emit("make-turn", { move: "reshuffle" });
+};
+>>>>>>> Stashed changes
 
 const reshuffleAll = () => {
-    // somebody reshuffled 
-}
+  // somebody reshuffled
+};
 
 const endTurn = () => {
+<<<<<<< Updated upstream
     sock.emit('end-turn', null);
 }
 
@@ -226,3 +281,38 @@ sock.on('card-thrown-all', cardThrownAll)
 let game = new Game();
 game.startGame();
 >>>>>>> 3395be2bc313910fc040a9c7bc3721ca1014485d
+=======
+  sock.emit("end-turn", null);
+};
+
+const error = err => {
+  if (err.msg === "Not your turn!") {
+    console.log(err.msg);
+  }
+};
+
+const cardThrownAll = card => {
+  console.log("card was thrown on deck " + card);
+};
+
+document.getElementById("name-form").addEventListener("submit", registerName);
+// document
+//   .getElementsByClassName("new-card")[0]
+//   .addEventListener("click", initNewCard);
+document
+  .getElementsByClassName("reshuffle")[0]
+  .addEventListener("click", initReshuffle);
+document
+  .getElementsByClassName("end-turn")[0]
+  .addEventListener("click", endTurn);
+
+sock.on("registerRes", registerSuccessRes);
+sock.on("message", receiveMsgToAll);
+sock.on("error-msg", error);
+
+// sock.on("new-card", newCard);
+sock.on("new-card-all", newCardAll);
+
+sock.on("reshuffle", reshuffleAll);
+sock.on("card-thrown-all", cardThrownAll);
+>>>>>>> Stashed changes
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const User = require("./user.js");
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 const http = require("http");
 const express = require("express");
@@ -8,6 +9,11 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 >>>>>>> 3395be2bc313910fc040a9c7bc3721ca1014485d
+=======
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+>>>>>>> Stashed changes
 const app = express();
 let availableCards = [
   "2C",
@@ -75,6 +81,7 @@ const io = socketio(server);
 let players = []; // array of Users
 let settingActivePlayer = false;
 
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 const findPlayerById = id => {
   return players.find(x => x.sockid === id);
@@ -88,20 +95,28 @@ const registerUser = (id, name) => {
   const user = new User(id, name);
 =======
 const findPlayerById = (id) => {
+=======
+const findPlayerById = id => {
+>>>>>>> Stashed changes
   return players.find(x => x.sockid === id);
-}
+};
 
-const sendMsgToAll = (msg) => {
-  io.emit('message', { text: msg, date: new Date() });
-}
+const sendMsgToAll = msg => {
+  io.emit("message", { text: msg, date: new Date() });
+};
 
 const registerUser = (id, name) => {
+<<<<<<< Updated upstream
   const user = new User(id, name)
 >>>>>>> 3395be2bc313910fc040a9c7bc3721ca1014485d
+=======
+  const user = new User(id, name);
+>>>>>>> Stashed changes
 
   if (players.length === 0) user.activeTurn = true;
 
   players.push(user);
+<<<<<<< Updated upstream
 <<<<<<< HEAD
   console.log("registered: " + user.sockid);
   io.to(user.sockid).emit("registerRes", "You are registered " + user.name);
@@ -142,31 +157,48 @@ const setNextActivePlayer = player => {
 =======
   console.log("registered: " + user.sockid)
   io.to(user.sockid).emit('registerRes', 'You are registered ' + user.name);
+=======
+  console.log("registered: " + user.sockid);
+  io.to(user.sockid).emit("registerRes", "You are registered " + user.name);
+>>>>>>> Stashed changes
 
   sendMsgToAll(`${name} was registered!`);
-}
+};
 
-const sendNewCardToPlayer = (player) => {
+const sendNewCardToPlayer = player => {
+  if (availableCards.length === 0) {
+    io.to(player.sockid).emit("new-card", {
+      cardCode: availableCards[i],
+      success: false
+    });
+    return;
+  }
+
   // chose card
-  const i = number = Math.floor(Math.random() * this._availableCards.length);
+  const i = (number = Math.floor(Math.random() * availableCards.length));
 
   // send card to a player
-  io.to(player.sockid).emit('new-card', { cardCode: availableCards[i] });
+  io.to(player.sockid).emit("new-card", {
+    cardCode: availableCards[i],
+    success: true
+  });
   availableCards.splice(number, 1);
 
   // send message to everyone so they can change view
-  io.emit('new-card-all', null);
-  sendMsgToAll(`${player.name} took a new card!`)
-}
+  io.emit("new-card-all", null);
+  sendMsgToAll(`${player.name} took a new card!`);
+};
 
-const reshuffleCards = (player) => {
+const reshuffleCards = player => {
   // to do: reshuffle
-  sendMsgToAll(`${player.name} reshuffled!`)
-}
+  sendMsgToAll(`${player.name} reshuffled!`);
+};
 
-const setNextActivePlayer = (player) => {
-
+<<<<<<< Updated upstream
 >>>>>>> 3395be2bc313910fc040a9c7bc3721ca1014485d
+=======
+const setNextActivePlayer = player => {
+>>>>>>> Stashed changes
   let activeIndex = -1;
 
   players.forEach((p, i) => {
@@ -174,6 +206,7 @@ const setNextActivePlayer = (player) => {
       activeIndex = i;
       p.activeTurn = false;
     }
+<<<<<<< Updated upstream
 <<<<<<< HEAD
   });
 
@@ -236,62 +269,77 @@ io.on("connection", sock => {
     if (typeof player !== "undefined" && player !== null) {
 =======
   })
+=======
+  });
+>>>>>>> Stashed changes
 
   if (activeIndex === -1 && players.length !== 0) {
     players[0].activeTurn = true;
-    console.log("tura gracza 1" + players[0].activeTurn)
+    console.log("tura gracza 1" + players[0].activeTurn);
   } else if (activeIndex === players.length - 1) {
     players[0].activeTurn = true;
-    console.log(" tura gracza nr 1" + players[0].activeTurn)
+    console.log(" tura gracza nr 1" + players[0].activeTurn);
   } else {
     players[activeIndex + 1].activeTurn = true;
-    console.log("tura gracza " + (activeIndex + 1) + players[0].activeTurn + " | " + + players[1].activeTurn)
+    console.log(
+      "tura gracza " +
+        (activeIndex + 1) +
+        players[0].activeTurn +
+        " | " +
+        +players[1].activeTurn
+    );
   }
-}
+};
 
-const setNextActivePlayerMonitor = (player) => {
+const setNextActivePlayerMonitor = player => {
   // to do przeniesc to na koniec ruchu uzytkownika albo przy disconneccie aktywnego
-  while (settingActivePlayer === true) { }
+  while (settingActivePlayer === true) {}
   settingActivePlayer = true;
   setNextActivePlayer(player);
   settingActivePlayer = false;
-}
+};
 
-io.on('connection', (sock) => {
+io.on("connection", sock => {
   let player;
-  console.log("someone connected", sock.id)
+  console.log("someone connected", sock.id);
 
-  sock.on('name', name => {
+  sock.on("name", name => {
     player = findPlayerById(sock.id);
     registerUser(sock.id, name);
-  })
+  });
 
-  sock.on('make-turn', turnData => {
+  sock.on("make-turn", turnData => {
     player = findPlayerById(sock.id);
-    if (typeof player === 'undefined' || player === null) {
-      console.log('Error: there is no player as registered')
-    } else if (player.activeTurn === true) { // turn can only be played fore on user at the time
-      if (turnData.move === 'new-card') {
+    if (typeof player === "undefined" || player === null) {
+      console.log("Error: there is no player as registered");
+    } else if (player.activeTurn === true) {
+      // turn can only be played fore on user at the time
+      if (turnData.move === "new-card") {
         sendNewCardToPlayer(player);
-      } else if (turnData.move === 'reshuffle') {
+      } else if (turnData.move === "reshuffle") {
         reshuffleCards(player);
       } else {
-        console.log('Error: there is no code for this move')
+        console.log("Error: there is no code for this move");
       }
     } else {
-      sock.emit('error-msg', { msg: 'Not your turn!' })
+      sock.emit("error-msg", { msg: "Not your turn!", success: false });
     }
-  })
+  });
 
-  sock.on('end-turn', () => {
+  sock.on("end-turn", () => {
     player = findPlayerById(sock.id);
 
+<<<<<<< Updated upstream
     if (typeof player !== 'undefined' && player !== null) {
 >>>>>>> 3395be2bc313910fc040a9c7bc3721ca1014485d
+=======
+    if (typeof player !== "undefined" && player !== null) {
+>>>>>>> Stashed changes
       if (player.activeTurn === true) {
         player = findPlayerById(sock.id);
         setNextActivePlayerMonitor(player);
       } else {
+<<<<<<< Updated upstream
 <<<<<<< HEAD
         sock.emit("error-msg", { msg: "Not your turn!", success: false });
       }
@@ -305,22 +353,31 @@ io.on('connection', (sock) => {
       sendMsgToAll(player.name + " disconnected!");
 =======
         sock.emit('error-msg', { msg: 'Not your turn!' })
+=======
+        sock.emit("error-msg", { msg: "Not your turn!", success: false });
+>>>>>>> Stashed changes
       }
     }
-  })
+  });
 
-  sock.on('disconnect', () => {
+  sock.on("disconnect", () => {
     player = findPlayerById(sock.id);
 
+<<<<<<< Updated upstream
     if (typeof player !== 'undefined' && player !== null) {
       sendMsgToAll(player.name + " disconnected!")
 >>>>>>> 3395be2bc313910fc040a9c7bc3721ca1014485d
+=======
+    if (typeof player !== "undefined" && player !== null) {
+      sendMsgToAll(player.name + " disconnected!");
+>>>>>>> Stashed changes
       if (player.activeTurn === true) setNextActivePlayerMonitor(player);
 
       // remove player from array
       let i = players.indexOf(player);
       players.splice(i, 1);
     }
+<<<<<<< Updated upstream
 <<<<<<< HEAD
   });
 
@@ -348,31 +405,37 @@ server.listen(8080, () => {
 
 =======
   })
+=======
+  });
+>>>>>>> Stashed changes
 
-  sock.on('throw-card', (card) => {
-    console.log('rzucona')
+  sock.on("throw-card", card => {
+    console.log("rzucona");
     player = findPlayerById(sock.id);
 
-    if (typeof player !== 'undefined' && player !== null) {
+    if (typeof player !== "undefined" && player !== null) {
       if (!cardsOnDeck.includes(card) && !availableCards.includes(card)) {
         cardsOnDeck.push(card);
-        io.emit('card-thrown-all');
-        sendMsgToAll(`${player.name} played ${card}`)
+        io.emit("card-thrown-all");
+        sendMsgToAll(`${player.name} played ${card}`);
       }
     }
-  })
-})
+  });
+});
 
-server.on('error', (err) => {
-  console.error('Server error:', err);
+server.on("error", err => {
+  console.error("Server error:", err);
 });
 
 server.listen(8080, () => {
-  console.log('server started on http://localhost:8080');
+  console.log("server started on http://localhost:8080");
 });
 
+<<<<<<< Updated upstream
 
 >>>>>>> 3395be2bc313910fc040a9c7bc3721ca1014485d
+=======
+>>>>>>> Stashed changes
 // const express = require("express");
 // const app = express();
 // const path = require("path");
